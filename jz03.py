4c77ae6740088e69e3ac8fbdf985480b3fff8beb
Yikouniao <chinoandtippi@gmail.com>
2021-08-25 14:53:41 +0800

    Update jz03.py

--- jz03.py
+++ jz03.py
@@ -2,11 +2,22 @@
 # https://leetcode-cn.com/problems/shu-zu-zhong-zhong-fu-de-shu-zi-lcof/
 
 class Solution:
+    # def findRepeatNumber(self, nums: List[int]) -> int:
+    #     d = {}
+    #     for num in nums:
+    #         if num not in d:
+    #             d[num] = 1
+    #         else:
+    #             return num
+    #     return -1    
+    
     def findRepeatNumber(self, nums: List[int]) -> int:
-        d = {}
-        for num in nums:
-            if num not in d:
-                d[num] = 1
-            else:
-                return num
-        return -1  
+        for i in range(len(nums)):
+            while i != nums[i]:
+                if nums[i] != nums[nums[i]]:
+                    t = nums[i]
+                    nums[i] = nums[t]
+                    nums[t] = t
+                else:
+                    return nums[i]
+        return -1
