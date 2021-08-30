52d1f4d852f827fe9682ccd7dd6d8a675a480db6
Yikouniao <chinoandtippi@gmail.com>
2021-08-30 13:51:11 +0800

    Update jz08.py

--- jz08.py
+++ jz08.py
@@ -6,13 +6,13 @@ class CQueue:
         self.s1, self.s2 = [], []
 
     def appendTail(self, value: int) -> None:
-        if not self.s1:
-            while self.s2:
-                self.s1.append(self.s2.pop())
         self.s1.append(value)
 
     def deleteHead(self) -> int:
         if not self.s2:
-            while self.s1:
-                self.s2.append(self.s1.pop())
-        return self.s2.pop() if self.s2 else -1
+            if not self.s1:
+                return -1
+            else:
+                while self.s1:
+                    self.s2.append(self.s1.pop())
+        return self.s2.pop()
