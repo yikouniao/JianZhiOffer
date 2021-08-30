87f12b05c9bb62e7d66653fe7f6f3267d20ef53f
Yikouniao <chinoandtippi@gmail.com>
2021-08-30 14:54:33 +0800

    Update jz09.py

--- jz09.py
+++ jz09.py
@@ -2,9 +2,9 @@
 
 class Solution:
     def fib(self, n: int) -> int:
-        table = [0, 1]
+        a, b = 0, 1
         if n < 2:
-            return table[n]
+            return n
         for i in range(2, n + 1):
-            table.append(table[-1] + table[-2])
-        return table[-1] % 1000000007
+            a, b = b, (a + b) % 1000000007
+        return b
