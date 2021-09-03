672ac5d20210dcbc33594c9a2cc72fadc158b3b9
Yikouniao <chinoandtippi@gmail.com>
2021-09-03 17:27:24 +0800

    Update jz26.py

--- jz26.py
+++ jz26.py
@@ -8,4 +8,13 @@ class TreeNode:
 
 class Solution:
     def isSubStructure(self, A: TreeNode, B: TreeNode) -> bool:
-      pass
+        if not A or not B:
+            return False
+        return self.dfs(A, B) or self.isSubStructure(A.left, B) or self.isSubStructure(A.right, B)
+
+    def dfs(self, A: TreeNode, B: TreeNode) -> bool:
+        if not B:
+            return True
+        if not A:
+            return False
+        return A.val == B.val and self.dfs(A.left, B.left) and self.dfs(A.right, B.right)
