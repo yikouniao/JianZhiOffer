aa4632009566bc8c1b9fe009dd6fc8dfacc2a3be
Yikouniao <chinoandtippi@gmail.com>
2021-09-03 10:20:24 +0800

    Create jz18.py

---
+++ jz18.py
@@ -0,0 +1,11 @@
+# https://leetcode-cn.com/problems/shan-chu-lian-biao-de-jie-dian-lcof/
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+        
+class Solution:
+    def deleteNode(self, head: ListNode, val: int) -> ListNode:
+      
