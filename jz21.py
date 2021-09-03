9c37a611b9d7efb3081498e2f3db2e3ae91ae9f4
Yikouniao <chinoandtippi@gmail.com>
2021-09-03 11:27:32 +0800

    Update jz21.py

--- jz21.py
+++ jz21.py
@@ -1,2 +1,7 @@
 # https://leetcode-cn.com/problems/diao-zheng-shu-zu-shun-xu-shi-qi-shu-wei-yu-ou-shu-qian-mian-lcof/
 
+class Solution:
+    def exchange(self, nums: List[int]) -> List[int]:
+        odd = [num for num in nums if num % 2]
+        even = [num for num in nums if not num % 2]
+        return odd + even
