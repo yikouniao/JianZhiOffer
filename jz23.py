4ee8cdcf4bb56456f7ecafc6ae8d859c4e216109
Yikouniao <chinoandtippi@gmail.com>
2021-09-03 15:50:17 +0800

    Update jz23.py

--- jz23.py
+++ jz23.py
@@ -1,2 +1,19 @@
 # https://leetcode-cn.com/problems/fan-zhuan-lian-biao-lcof/
 
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        if not head:
+            return head
+        p1, p2 = head, head.next
+        p1.next = None
+        while p2:
+            t = p2.next
+            p2.next = p1
+            p1 = p2
+            p2 = t
+        return p1
