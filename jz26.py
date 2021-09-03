6f4e47e431d2de63135316683ecfbaf398d53911
Yikouniao <chinoandtippi@gmail.com>
2021-09-03 16:34:49 +0800

    Create jz26.py

---
+++ jz26.py
@@ -0,0 +1,11 @@
+# https://leetcode-cn.com/problems/shu-de-zi-jie-gou-lcof/
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def isSubStructure(self, A: TreeNode, B: TreeNode) -> bool:
+      pass
