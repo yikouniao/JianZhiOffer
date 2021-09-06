62355adb3dd5ffe8ed80ffbd4feda9f97ad4ede8
Yikouniao <chinoandtippi@gmail.com>
2021-09-06 18:40:44 +0800

    Update jz31.py

--- jz31.py
+++ jz31.py
@@ -2,25 +2,17 @@
 
 class Solution:
     def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
-        n = len(pushed)
+        n, m = len(pushed), len(popped)
+        if n != m:
+            return False
         if n == 0:
             return True
         stack = []
-        p_pushed, p_popped = 0, 0
-        while p_pushed < n and p_popped < n:
-            if len(stack) > 0 and p_popped < n and stack[-1] == popped[p_popped]:
+        p = 0
+        for e in pushed:
+            stack.append(e)
+            while len(stack) > 0 and p < n and stack[-1] == popped[p]:
+                p += 1
                 stack.pop()
-                p_popped += 1
-            elif pushed[p_pushed] == popped[p_popped]:
-                p_pushed, p_popped = p_pushed + 1, p_popped + 1
-            else:
-                stack.append(pushed[p_pushed])
-                p_pushed += 1
-        if p_popped < n:
-            p_stack = len(stack) - 1
-            while p_stack >= 0 and p_popped < n:
-                if stack[p_stack] != popped[p_popped]:
-                    return False
-                else:
-                    p_stack, p_popped = p_stack - 1, p_popped + 1
-        return True
+
+        return p == n
