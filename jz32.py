b7b9628d9ad1bab34c7c8f2e33a7b4e025e736e2
Yikouniao <chinoandtippi@gmail.com>
2021-09-06 18:41:44 +0800

    Create jz32.py

---
+++ jz32.py
@@ -0,0 +1,12 @@
+# https://leetcode-cn.com/problems/cong-shang-dao-xia-da-yin-er-cha-shu-lcof/
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[int]:
+        pass    
