a76d8936790246b659e2ed76c4dfc2401c06c03c
Yikouniao <chinoandtippi@gmail.com>
2021-09-07 13:58:53 +0800

    Create jz34.py

---
+++ jz34.py
@@ -0,0 +1,13 @@
+# https://leetcode-cn.com/problems/er-cha-shu-zhong-he-wei-mou-yi-zhi-de-lu-jing-lcof/
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def pathSum(self, root: TreeNode, target: int) -> List[List[int]]:
+        pass
