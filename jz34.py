7c5bbc736b16ef2d6dafb61ac88f82910b2b2c54
Yikouniao <chinoandtippi@gmail.com>
2021-09-07 15:53:28 +0800

    Update jz34.py

--- jz34.py
+++ jz34.py
@@ -10,4 +10,19 @@ class TreeNode:
 
 class Solution:
     def pathSum(self, root: TreeNode, target: int) -> List[List[int]]:
-        pass
+        results = []
+        record = []
+        self.dfs(results, root, target, record)
+        return results
+
+    def dfs(self, results, root, target, record):
+        if not root:
+            return
+        record.append(root.val)
+        if not root.left and not root.right and target == root.val:
+            results.append([_ for _ in record])
+        else:
+            target -= root.val
+            self.dfs(results, root.left, target, record)
+            self.dfs(results, root.right, target, record)
+        record.pop()
