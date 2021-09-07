1d7788e7565c457cb89ae417be896109ad7f4332
Yikouniao <chinoandtippi@gmail.com>
2021-09-07 10:17:10 +0800

    Create jz32-2.py

---
+++ jz32-2.py
@@ -0,0 +1,9 @@
+# https://leetcode-cn.com/problems/cong-shang-dao-xia-da-yin-er-cha-shu-ii-lcof/
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
