fdb498e4954833fba0ff207c1569bca332380fdf
Yikouniao <chinoandtippi@gmail.com>
2021-09-15 11:19:54 +0800

    Update jz40.py

--- jz40.py
+++ jz40.py
@@ -3,18 +3,37 @@
 
 class Solution:
     def getLeastNumbers(self, arr: List[int], k: int) -> List[int]:
-        if k == 0:
+        n = len(arr)
+        topk = k
+        if k == 0 or n == 0:
             return []
-        result = []
-        max_p = 0
-        for a in arr:
-            if len(result) < k:
-                result.append(a)
-                if result[max_p] < result[-1]:
-                    max_p = len(result) - 1
-            elif result[max_p] > a:
-                result[max_p] = a
-                for i in range(k):
-                    if result[i] > result[max_p]:
-                        max_p = i
-        return result
+
+        i = n // 2
+        while i > 0:
+            self.adjust(arr, n, i)
+            i -= 1
+
+        while k > 0:
+            self.swap(arr, 1, n)
+            n -= 1
+            self.adjust(arr, n, 1)
+            k -= 1
+
+        return arr[-topk:]
+
+    def adjust(self, arr, length, i):
+        if 2 * i > length or i <= 0 or length <= 0:
+            return
+        elif 2 * i + 1 > length or arr[2 * i - 1] < arr[2 * i]:
+            if arr[2 * i - 1] < arr[i - 1]:
+                self.swap(arr, i, 2 * i)
+                self.adjust(arr, length, i * 2)
+        elif arr[2 * i] < arr[i - 1]:
+            self.swap(arr, i, 2 * i + 1)
+            self.adjust(arr, length, 2 * i + 1)
+        return
+
+    def swap(self, arr, i, j):
+        t = arr[i - 1]
+        arr[i - 1] = arr[j - 1]
+        arr[j - 1] = t
